fbc089f9d60a1115a04f5803972a8daac00c352c
k0na <pwpvpdru@yandex.ru>
2021-10-31 01:10:11 +0300

    HomeWork 6 done

---
+++ src/test/java/Cat.java
@@ -0,0 +1,27 @@
+public class Cat extends Animal {
+    private static int value;
+
+    public Cat(String name) {
+        super(name);
+        value++;
+    }
+
+    public void run(int runValue) {
+        if (runValue <= 0 ) {
+            System.out.println("Для того, чтобы кот бежал - введите значение от 1 до 200");
+        } else if (runValue > 200) {
+            System.out.println("Кот не может пробежать более 200 метров без отдыха");
+        } else {
+            System.out.println("Кот " + getName() + " пробежал " + runValue + " м.");
+        }
+    }
+
+    public void swim(int swimValue) {
+        System.out.println("Наш котик " + getName() + " плавает как топор, давайте не будем рисковать.");
+    }
+
+    @Override
+    public int getValue() {
+        return value;
+    }
+}
---
+++ src/test/java/Dog.java
@@ -0,0 +1,34 @@
+public class Dog extends Animal {
+
+    private static int value;
+
+    public Dog(String name) {
+        super(name);
+        value++;
+    }
+
+    public void run(int runValue) {
+        if (runValue <= 0 ) {
+            System.out.println("Для того, чтобы пес бежал - введите значение от 1 до 500");
+        } else if (runValue > 500) {
+            System.out.println("Пес не может пробежать более 500 метров без отдыха");
+        } else {
+            System.out.println("Пёс " + getName() + " пробежал " + runValue + " м.");
+        }
+    }
+
+    public void swim(int swimValue) {
+        if (swimValue <= 0 ) {
+            System.out.println("Для того, чтобы пес плыл - введите значение от 1 до 500");
+        } else if (swimValue > 10) {
+            System.out.println("Пес не может проплыть более 10 метров без отдыха");
+        } else {
+            System.out.println("Пёс " + getName() + " проплыл " + swimValue + " м.");
+        }
+    }
+
+    @Override
+    public int getValue() {
+        return value;
+    }
+}
